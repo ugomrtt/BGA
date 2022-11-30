a052fc48cb5aa29607b91301fff1a7c6cb718014
Ugo Mariotto <ugo.mariotto@etu.iut-tlse3.fr>
2022-11-30 14:02:28 +0100

    Initial Commit

---
+++ BGADream/BGADreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGADreamApp.swift
+//  BGADream
+//
+//  Created by Ugo Mariotto on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BGADreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
